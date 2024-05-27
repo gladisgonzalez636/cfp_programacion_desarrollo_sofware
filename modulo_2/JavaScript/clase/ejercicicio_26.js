cb7297e1fbfddf3ddca6ca3fe213988551986aae
Gladis Jeaneth González Cúmes <gladisgonzalez@CFPBARTOLOMEAMBROSIO.COM>
2024-05-27 16:00:03 -0600

    cambios

---
+++ modulo_2/JavaScript/clase/ejercicicio_26.js
@@ -0,0 +1,11 @@
+let resultado = Math.floor( Math.random() * (10 - 1) + 1);
+console.log(resultado);
+
+do{ 
+    resultado = Math.floor( Math.random() * (-10 - 10) + 1);
+    console.log(resultado);
+}while(resultado > 0)
+
+console.log('positivo');
+
+
---
+++ modulo_2/JavaScript/clase/ejercicio_25.js
@@ -0,0 +1,8 @@
+let usuario = 123456;
+let contraseña = 123456;
+while (usuario!=contraseña){
+    console.log('incorrecto');
+    if(usuario==contraseña){
+        console.log('correcto');
+    }
+} 
